feb6e09194f69fb934a40f640fe0222f3d5614b0
Rednam-madhavi <rednammadhavi@gmail.com>
2025-02-24 16:25:16 +0530

    Completed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,9 +5,9 @@ export default class Navbar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top ">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top ">
                     <div className="container-fluid">
-                        <Link className="navbar-brand" to="/">EchoNews</Link>
+                        <Link className="navbar-brand text-danger fw-bold" to="/">EchoNews</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
